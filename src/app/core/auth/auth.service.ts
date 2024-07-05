5e8af850fbd5551e90750730a028cc5ac67ccada
Pham Thanh Hung <thanhhungsky98@gmail.com>
2024-07-06 01:41:39 +0700

    123

--- src/app/core/auth/auth.service.ts
+++ src/app/core/auth/auth.service.ts
@@ -7,6 +7,8 @@ import { environment } from 'environments/environment';
 import { catchError, Observable, of, switchMap, throwError } from 'rxjs';
 import { user as userData } from 'app/mock-api/common/user/data';
 import { cloneDeep } from 'lodash-es';
+import { FeedbackService } from 'app/services/feeback.service';
+import { TokenExpiredService } from 'app/services/tokenexpired.service';
 
 const authUrl = environment.idApiUrl + 'auth';
 
@@ -15,7 +17,7 @@ export class AuthService {
     private _authenticated: boolean = false;
     private _httpClient = inject(HttpClient);
     private _userService = inject(UserService);
-    private _userApiService = inject(UserApiService)
+    private _tokenService = inject(TokenExpiredService)
 
     // -----------------------------------------------------------------------------------------------------
     // @ Accessors
@@ -84,43 +86,36 @@ export class AuthService {
      * @param credentials
      */
     signIn(credentials: { userName: string; password: string }): Observable<any> {
-        // Throw error if the user is already logged in
-        if (this._authenticated) {
-            return throwError('User is already logged in.');
+        if (this.accessToken) {
+            location.reload();
+        } else {
+            return this._httpClient.post(`${authUrl}/login`, credentials).pipe(
+                switchMap((response: any) => {
+                    const headers = new HttpHeaders({
+                        'Authorization': `Bearer ${response.accessToken}`
+                    });
+                    // Make a GET request to the /manage/info endpoint with the access token in the headers
+                    return this._httpClient.get(`${authUrl}/manage/info`, { headers: headers }).pipe(
+                        switchMap((userInfo: any) => {
+                            // Store the user on the user service
+                            this._userService.user = userInfo;
+
+                            this.accessToken = response.accessToken;
+                            this.setExpireDate(response.expiresIn);
+                            this.refreshToken = response.refreshToken;
+                            // Set the authenticated flag to true
+                            this._authenticated = true;
+
+                            localStorage.setItem('tenantId', userInfo.tenantId);
+                            localStorage.setItem('role', userInfo.role);
+                            localStorage.setItem('userId', userInfo.id);
+
+                            return of(userInfo);
+                        })
+                    );
+                })
+            );
         }
-    
-        return this._httpClient.post(`${authUrl}/login`, credentials).pipe(
-            switchMap((response: any) => {
-                // Store the access token in the local storage
-    
-                // Prepare the headers with the access token
-                const headers = new HttpHeaders({
-                    'Authorization': `Bearer ${response.accessToken}`
-                });
-                console.log(headers);
-                // Make a GET request to the /manage/info endpoint with the access token in the headers
-                return this._httpClient.get(`${authUrl}/manage/info`, { headers: headers }).pipe(
-                    switchMap((userInfo: any) => {
-                        // Store the user on the user service
-                        this._userService.user = userInfo;
-    
-                        this.accessToken = response.accessToken;
-                        this.setExpireDate(response.expiresIn);
-                        this.refreshToken = response.refreshToken;
-                        // Set the authenticated flag to true
-                        this._authenticated = true;
-                        // Return a new observable with the response
-
-                        // set tenantId and role and userId to local storage
-                        localStorage.setItem('tenantId', userInfo.tenantId);
-                        localStorage.setItem('role', userInfo.role);
-                        localStorage.setItem('userId', userInfo.id);
-
-                        return of(userInfo);
-                    })
-                );
-            })
-        );
     }
 
     /**
@@ -167,6 +162,8 @@ export class AuthService {
      * Sign out
      */
     signOut(): Observable<any> {
+        this._tokenService.create({ token: this.accessToken }).subscribe((data) => {
+        });
         // Remove the access token from the local storage
         localStorage.removeItem('accessToken');
         localStorage.removeItem('expireDate');
